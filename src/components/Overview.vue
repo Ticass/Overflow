0393ea5bd6d970a5aa13a80103ce4597bc28b3dd
Alexis <thrifthshopdowntheroad@gmail.com>
2020-10-24 12:52:25 -0400

    :sparkles: Added Basic stuff

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="overview">
-        <h3 class="ram">Current Ram: {{ ram }}</h3>
+        <h3>Current Ram</h3>
+        <div class="ram">{{ ram }}</div>
         <div class="jumbotron">
         <div class="rps"> Ram Per Second: {{ rps }}</div>
         <div class="level">Current Level: {{ level }}</div>
@@ -30,3 +31,35 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+.overview {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 25px;
+}
+
+    h3 {
+        color: #222;
+        font-size: 22px;
+        font-weight: 400;
+        margin-bottom: 15px;
+    }
+    
+    .ram {
+        width: 100%;
+        min-width: 200px;
+        max-width: fit-content;
+        padding: 15px 25px;
+        background-color: #28A484;
+        border-radius: 8px;
+
+        color: #FFF;
+        font-size: 60px;
+        font-weight: 900;
+        text-align: center;
+        margin-bottom: 30px;
+    }
+
+</style>
